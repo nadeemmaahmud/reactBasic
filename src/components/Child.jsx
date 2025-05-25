53f5cf1544c061b5a155118533e17b6851f9a0e5
Nadim Mahmud <99585699+nadeemmaahmud@users.noreply.github.com>
2025-05-25 20:36:26 +0600

    Update Child.jsx

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -5,12 +5,16 @@ function Child() {
     let [count, setCount] = useState(0)
 
     return (
-        <div>
-            <h2>Counter : {count}</h2>
-            <button className='btn btn-primary me-2' onClick={() => {setCount(count + 1)}}>Count by One</button>
-            <button className='btn btn-danger' onClick={() => {setCount(0)}}>Reset</button>
-        </div>
-    )
+    <div>
+      <h1>Counter App</h1>
+      <hr/>
+
+      <h3>Counted : {counter}</h3>
+      <button type='submit' className='btn btn-primary me-2' onClick={() => {setCount(counter + 1)}}>Count++</button>
+      <button type='submiy' className='btn btn-warning me-2' onClick={() => {setCount(counter - 1)}}>Count--</button>
+      <button type='submiy' className='btn btn-danger' onClick={() => {setCount(0)}}>Reset</button>
+    </div>
+  )
 }
 
 export default Child
